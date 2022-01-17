239fb842b4c53f35b157fbf865bcfa224c47f51c
Viers <krusnik.viers@gmail.com>
2022-01-17 21:20:58 +0100

    Fix test and errcode

--- app/bot/config.py
+++ app/bot/config.py
@@ -16,28 +16,31 @@ class Config:
         self.admin_username = admin_user
         self.storage_dir = storage_dir
 
-    # Note, that this method will always _parse_ current configuration.
     @classmethod
-    def get(cls):
-        # Parse command line for config file path, if needed.
+    def _parse_config_file(cls, config_path: str):
+        config_path = pathlib.Path(config_path).resolve()
+        assert config_path.is_file()
+        ini_parser = configparser.ConfigParser()
+        ini_parser.read(str(config_path))
+        default_section = ini_parser['DEFAULT']
+        # Add your values here
+        return cls(default_section.get('telegram_bot_token', ''),
+                   default_section.get('bot_admin_username', ''),
+                   cls._check_and_normalize_dir(default_section.get('storage_directory', str(DEFAULT_DB_PATH))))
+
+    @staticmethod
+    def _read_config_path_from_args() -> str:
         arg_parser = argparse.ArgumentParser(PROJECT_FULL_NAME)
         arg_parser.add_argument('--config', dest='config_path', type=str, default=str(DEFAULT_CONFIG_PATH),
                                 help='Path to the configuration .ini file')
         args = arg_parser.parse_args()
         logging.info('Configuration file set to {}'.format(args.config_path))
+        return args.config_path
 
-        # Parse config
-        config_path = pathlib.Path(args.config_path).resolve()
-        assert config_path.exists()
-        ini_parser = configparser.ConfigParser()
-        ini_parser.read(str(config_path))
-        default_section = ini_parser['DEFAULT']
-
-        # Create config instance
-        # Add your values here
-        return cls(default_section.get('telegram_bot_token', ''),
-                   default_section.get('bot_admin_username', ''),
-                   cls._check_and_normalize_dir(default_section.get('storage_directory', str(DEFAULT_DB_PATH))))
+    @classmethod
+    def get(cls):
+        config_path = cls._read_config_path_from_args()
+        return cls._parse_config_file(config_path)
 
     @staticmethod
     def _check_and_normalize_dir(dir_str: str) -> str:
