d89473b189d500a2dfa92ffbe1ca8f48b01cc15a
Viers <krusnik.viers@gmail.com>
2022-01-19 21:14:55 +0100

    Update public imports in template

--- app/internal/core/markup/callback_commands.py
+++ app/internal/core/markup/callback_commands.py
@@ -5,5 +5,7 @@ def encode_callback_data(data: list) -> str:
     return ' '.join([str(x) for x in data])
 
 
-def decode_callback_data(update: Update) -> tuple:
-    return tuple(update.callback_query.data.split()[1:])
+def decode_callback_data(update: Update, is_personal=False) -> tuple:
+    offset = 2 if is_personal else 1
+    data_split = update.callback_query.data.split()
+    return tuple(update.callback_query.data.split()[offset:]) if len(data_split) > offset else []
